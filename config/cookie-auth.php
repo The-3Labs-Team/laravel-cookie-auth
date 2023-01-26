ee3cce1ba6e455b9c68bb09d82be22cd8e25da99
Stefano Novelli <murdercode@gmail.com>
2023-01-26 12:14:19 +0100

    wip

--- config/cookie-auth.php
+++ config/cookie-auth.php
@@ -3,6 +3,6 @@
 // config for The3LabsTeam/LaravelCookieAuth
 return [
     'cookie_name' => 'skip_cache',
-    'cookie_value' => true,
-    'cookie_lifetime' => 5,
+    'cookie_value' => 'true',
+    'cookie_lifetime' => 15,
 ];
