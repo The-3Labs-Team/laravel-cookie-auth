891a294dd50e49fe6bed7234f18b351d3d96c0da
Stefano Novelli <murdercode@gmail.com>
2023-02-20 12:00:45 +0100

    Better function + check if cookie exists

--- src/LaravelCookieAuth.php
+++ src/LaravelCookieAuth.php
@@ -3,6 +3,8 @@
 namespace The3LabsTeam\LaravelCookieAuth;
 
 use Closure;
+use http\Client\Response;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class LaravelCookieAuth
@@ -10,31 +12,30 @@ class LaravelCookieAuth
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(Request): (\Illuminate\Http\Response|RedirectResponse) $next
+     * @return Response
      */
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
 
         if (auth()->check()) {
-            $response->withCookie(
-                cookie(
-                    config('cookie-auth.cookie_name'),
-                    config('cookie-auth.cookie_value'),
-                    config('cookie-auth.cookie_lifetime')
-                )
+            $cookie = cookie(
+                config('cookie-auth.cookie_name'),
+                config('cookie-auth.cookie_value'),
+                config('cookie-auth.cookie_lifetime')
             );
 
             if ($response->isRedirection()) {
-                $response->withCookie(
-                    cookie(
-                        config('cookie-auth.cookie_name'),
-                        config('cookie-auth.cookie_value'),
-                        config('cookie-auth.cookie_lifetime')
-                    )
-                );
+                $response->withCookie($cookie);
+            } else {
+                $response->cookie($cookie);
+            }
+
+            if (!$response->headers->getCookies() || !$response->headers->getCookies()[0]->getValue(
+                )) {
+                return new Response('Unable to set skip_cache cookie', 500);
             }
         }
 
